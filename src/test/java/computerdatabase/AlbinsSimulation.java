0a420286535f004767110df17de71554537e5a2f
albihasani94 <albinhasani@protonmail.com>
2022-12-29 11:23:10 +0100

    Code refactoring

--- src/test/java/computerdatabase/AlbinsSimulation.java
+++ src/test/java/computerdatabase/AlbinsSimulation.java
@@ -1,16 +1,11 @@
 package computerdatabase;
 
 
-import java.time.Duration;
-import java.util.*;
-
 import io.gatling.javaapi.core.*;
 import io.gatling.javaapi.http.*;
-import io.gatling.javaapi.jdbc.*;
 
 import static io.gatling.javaapi.core.CoreDsl.*;
 import static io.gatling.javaapi.http.HttpDsl.*;
-import static io.gatling.javaapi.jdbc.JdbcDsl.*;
 
 public class AlbinsSimulation extends Simulation {
 
@@ -24,54 +19,50 @@ public class AlbinsSimulation extends Simulation {
         .upgradeInsecureRequestsHeader("1")
         .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36");
 
-    private ScenarioBuilder scn = scenario("AlbinsSimulation")
-        .exec(
-            http("request_0")
+    ChainBuilder searchForComputer =
+        exec(
+            http("Load home page")
                 .get("/computers")
         )
-        .pause(3)
-        .exec(
-            http("request_1")
-                .get("/computers?f=macbook")
-        )
-        .pause(2)
-        .exec(
-            http("request_2")
-                .get("/computers/517")
-        )
-        .pause(18)
-        .exec(
-            http("request_3")
-                .post("/computers/517")
-                .formParam("name", "MacBook 13-inch Core 2 Duo 2.13GHz (MC240LL/A) DDR2 Model")
-                .formParam("introduced", "2013-12-10")
-                .formParam("discontinued", "2020-12-10")
-                .formParam("company", "1")
-        )
-        .pause(16)
-        .exec(
-            http("request_4")
-                .get("/computers?p=1&n=10&s=name&d=asc")
-        )
-        .pause(2)
-        .exec(
-            http("request_5")
-                .get("/computers?p=2&n=10&s=name&d=asc")
-        )
-        .pause(7)
-        .exec(
-            http("request_6")
+            .exec(
+                http("Search for a computer")
+                    .get("/computers?f=macbook")
+            )
+            .exec(
+                http("Load a specific computer")
+                    .get("/computers/517")
+            )
+            .exec(
+                http("Edit specific computer")
+                    .post("/computers/517")
+                    .formParam("name", "MacBook 13-inch Core 2 Duo 2.13GHz (MC240LL/A) DDR2 Model")
+                    .formParam("introduced", "2013-12-10")
+                    .formParam("discontinued", "2020-12-10")
+                    .formParam("company", "1")
+            );
+
+    ChainBuilder createComputer =
+        exec(
+            http("Load page to create a new computer")
                 .get("/computers/new")
         )
-        .pause(20)
-        .exec(
-            http("request_7")
-                .post("/computers")
-                .formParam("name", "Albin's Computer")
-                .formParam("introduced", "2022-12-22")
-                .formParam("discontinued", "2028-12-22")
-                .formParam("company", "2")
-        );
+            .exec(
+                http("Create a new computer")
+                    .post("/computers")
+                    .formParam("name", "Albin's Computer")
+                    .formParam("introduced", "2022-12-22")
+                    .formParam("discontinued", "2028-12-22")
+                    .formParam("company", "2")
+            );
+
+    ChainBuilder browse = repeat(5, "n").on(
+        exec(http("Page #{n}")
+            .get("/computers?p=#{n}"))
+            .pause(2)
+    );
+
+    private ScenarioBuilder scn = scenario("AlbinsSimulation")
+        .exec(searchForComputer, browse, createComputer);
 
     {
         setUp(scn.injectOpen(atOnceUsers(1))).protocols(httpProtocol);
